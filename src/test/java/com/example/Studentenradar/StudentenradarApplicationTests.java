2dc9259fe870f204f7ad9beae398820d0d6e2885
Arseit01 <Arbresha02@hotmailde>
2023-11-25 16:55:27 +0100

    Projekt Start

---
+++ src/test/java/com/example/Studentenradar/StudentenradarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Studentenradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentenradarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
