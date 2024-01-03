eb3d7342ede0d3e172177bc61bbfe59c39d474be
St3bz <nisehe14@gmail.com>
2024-01-03 16:09:02 +0100

    Improvenent

--- frontend/src/pages/Searchbar.js
+++ frontend/src/pages/Searchbar.js
@@ -1,25 +1,6 @@
 import search from '../images/search_icon.png';
-import React, {useState, useEffect} from 'react';
 
 const Searchbar = () => {
-
-    const [search, setSearch] = useState([])
-    const fetchSearchData = () => {
-        fetch('http://localhost:8081/api/v1/projects', {
-            method: 'GET',
-        })
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-            setSearch(data);
-            console.log('suche');
-            console.log(data)
-        })
-    }
-    useEffect(() => {
-        fetchSearchData();
-    }, [])
 
     return (  
         <div className="searchbar">
