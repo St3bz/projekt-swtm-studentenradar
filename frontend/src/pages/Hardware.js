d5b6d57deb841725adca1880bced1df4b4e67950
St3bz <nisehe14@gmail.com>
2024-01-06 23:30:10 +0100

    Added Searchbar to Hardware and Students

--- frontend/src/pages/Hardware.js
+++ frontend/src/pages/Hardware.js
@@ -1,11 +1,16 @@
 import React, {useState, useEffect} from 'react';
 import sort from '../images/sort.png';
 import selection from '../images/selection.png';
-import Searchbar from './Searchbar';
+import Searchbar from './HardwareSearchbar';
+import { useNavigate } from 'react-router-dom';
 
 const Hardware = () => {
 
   const [hardware, setHardware] = useState([])
+  const [filteredHardware, setFilteredHardware] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
   const fetchHardwareData = () => {
         fetch('http://localhost:8081/api/v1/hardware', {
             method: 'GET',
@@ -24,13 +29,53 @@ const Hardware = () => {
         /* fetchStudentData() */
     }, [])
 
-    
+    const handleSearch = (searchTerm) => {
+      setSearchTerm(searchTerm);
+
+      if (searchTerm.trim() === '') {
+          setFilteredHardware([]);
+      return;
+    }
+
+    const filtered = hardware.filter(
+      (hardware) => hardware.article.toLowerCase().includes(searchTerm.toLowerCase()));
+      setFilteredHardware(filtered);
+    };
+
+    const handleSearchSuggestions = (searchTerm) => {
+  
+      const suggestions = hardware.filter(
+      (hardware) => hardware.article.toLowerCase().includes(searchTerm.toLowerCase()));
+      setFilteredHardware(suggestions);
+    };
+
+    const renderResults = () => {
+      if(!searchTerm) return null;
+      if (filteredHardware.length === 0) {
+          return <p>No matching teams found for "{searchTerm}".</p>;
+      } else {
+          return (
+              <div>
+                  <p>Matching teams for "{searchTerm}":</p>
+                  <ul>
+                      {filteredHardware.map((hardware) => (
+                      <li key={hardware.id} onClick={() => navigate(`/hardware/${hardware.id}`)} >
+                          {hardware.article}
+                      </li>
+                      ))}
+                  </ul>
+              </div>
+          );
+      }
+    };
+
     return ( 
         <div>
           <div className="title">
             <b>Hardware</b>
             </div>
-          <Searchbar />
+          <Searchbar handleSearch={handleSearch} handleSuggestions={handleSearchSuggestions}/>
+          {renderResults()}
         <div className="auswahl">
         <img src={sort} className="sort" alt="logo" />  
         <a href="HardwareEdit">
--- frontend/src/pages/Students.js
+++ frontend/src/pages/Students.js
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import sort from '../images/sort.png';
 import selection from '../images/selection.png';
-import Searchbar from './Searchbar';
+import Searchbar from './StundentsSearchbar';
 import { Link } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import useFetch from './useFetch';
 
 const Students = () => {
@@ -11,6 +11,10 @@ const Students = () => {
   /*const { data: students, error, isPending } = useFetch('http://localhost:8081/api/v1/students' + id);
   console.log(students);*/
   const [students, setStudents] = useState([])
+  const [filteredStudents, setFilteredStudents] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
   const fetchStudentData = () => {
         fetch('http://localhost:8081/api/v1/students', {
             method: 'GET',
@@ -29,12 +33,57 @@ const Students = () => {
         /* fetchStudentData() */
     }, [])
 
+  const handleSearch = (searchTerm) => {
+      setSearchTerm(searchTerm);
+
+      if (searchTerm.trim() === '') {
+          setFilteredStudents([]);
+      return;
+  }
+
+  const filtered = students.filter(
+    (student) =>
+      student.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      student.lastName.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredStudents(filtered);
+  };
+
+  const handleSearchSuggestions = (searchTerm) => {
+    const suggestions = students.filter((student) =>
+      student.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      student.lastName.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredStudents(suggestions);
+  };
+
+  const renderResults = () => {
+      if(!searchTerm) return null;
+      if (filteredStudents.length === 0) {
+          return <p>No matching teams found for "{searchTerm}".</p>;
+      } else {
+          return (
+              <div>
+                  <p>Matching teams for "{searchTerm}":</p>
+                  <ul>
+                      {filteredStudents.map((student) => (
+                      <li key={student.id} onClick={() => navigate(`/profile/${student.id}`)} >
+                          {student.firstName} {student.lastName}
+                      </li>
+                      ))}
+                  </ul>
+              </div>
+          );
+      }
+  };
+
   return (
     <div>
       <div className="title">
         <b>Studenten</b>
       </div>
-      <Searchbar />
+      <Searchbar handleSearch={handleSearch} handleSuggestions={handleSearchSuggestions}/>
+      {renderResults()}
       <div className="auswahl">
         <img src={sort} className="sort" alt="logo" />
         <Link to="/StudentsEdit">
